2d39c1f1110b7455e8665323d7fcd7a07f2cf201
patrickamartins <patrick.amartins@gmail.com>
2025-05-14 15:35:24 -0300

    fix: adiciona @tailwindcss/postcss e postcss.config.js

--- scoremvp-frontend/postcss.config.js
+++ scoremvp-frontend/postcss.config.js
@@ -1,6 +1,7 @@
 // scoremvp-frontend/postcss.config.js
 module.exports = {
   plugins: {
+    // em vez de usar `tailwindcss` diretamente, usamos o pacote separado
     '@tailwindcss/postcss': {},
     autoprefixer: {},
   },
