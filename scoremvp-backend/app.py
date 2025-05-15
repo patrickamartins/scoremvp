7909034acb2475c806e1105cf78b7b5535bdbeba
patrickamartins <patrick.amartins@gmail.com>
2025-05-15 01:30:26 -0300

    ajuste app e docker

--- scoremvp-backend/app.py
+++ scoremvp-backend/app.py
@@ -8,5 +8,5 @@ def home():
     return "Hello from ScoreMVP!"
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Usa $PORT se existir, senão 5000
+    port = int(os.environ.get("PORT", 8080))  # Usa $PORT se existir, senão 5000
     app.run(host="0.0.0.0", port=port)
