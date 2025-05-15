dbd3b86078e9cdfbb432cea1a691711b1f430477
patrickamartins <patrick.amartins@gmail.com>
2025-05-14 21:35:03 -0300

    up all

--- scoremvp-frontend/src/App.tsx
+++ scoremvp-frontend/src/App.tsx
@@ -1,18 +1,15 @@
-// src/App.tsx
-import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import Login from './components/Login';
 import Dashboard from './components/Dashboard';
 import RegistroEstatisticas from './components/RegistroEstatisticas';
 
-const App: React.FC = () => (
-  <Routes>
-    <Route path="/login" element={<Login />} />
-    <Route path="/dashboard" element={<Dashboard />} />
-    <Route path="/register" element={<RegistroEstatisticas />} />
-    {/* redireciona tudo para /login */}
-    <Route path="*" element={<Navigate to="/login" replace />} />
-  </Routes>
-);
-
-export default App;
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/register" element={<RegistroEstatisticas />} />
+      <Route path="*" element={<Navigate to="/login" replace />} />
+    </Routes>
+  );
+}
