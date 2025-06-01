56a69aa9c619dc1a82b9b6f19a74a845c432a1fa
patrickamartins <patrick.amartins@gmail.com>
2025-06-01 16:31:44 -0300

    fix: remove AuthProvider do main.tsx para build público sem autenticação

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,12 +4,9 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import './utils/gtm';
-import { AuthProvider } from './contexts/AuthContext';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <App />
   </React.StrictMode>
 );
